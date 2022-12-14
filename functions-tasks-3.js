78bc64f02cb5242d6f9882ccb451ab644265091d
RaidasE30 <raidasb@gmail.com>
2022-12-14 17:53:10 +0200

    feat: solved tasks 29 to 32

--- functions-tasks-3.js
+++ functions-tasks-3.js
@@ -573,25 +573,37 @@ console.log();
 //  123456 -> false
 console.group("29. Sukurkite funkciją, kuri grąžina <true>, jeigu žodis yra palindromas (taip pat skaitosi iš abiejų pusių)");
 {
-    function isPalyndrome(str) {
-        let i = 0;
-        let k = str.length - 1;
-        while (i < k) {
-            if (str[i] !== str[k]) return false;
-            i++;
-            k--;
-        }
-        return true;
+    // Example below was provided by Žilvinas
+
+
+    // function isPalindrome(str) {
+    //     let i = 0;
+    //     let k = str.length - 1;
+    //     while (i < k) {
+    //         if (str[i] !== str[k]) return false;
+    //         i++;
+    //         k--;
+    //     }
+    //     return true;
+    // }
+
+    // my solution:
+    function isPalindrome(str) {
+        str = str.toLowerCase();
+
+        return str === str.split('').reverse().join('');
     }
 
-    // console.log('---');
-    // console.log({
-    //   'mama': isPalyndrome('mama'),
-    //   'mamam': isPalyndrome('mamam'),
-    //   '123321': isPalyndrome('123321'),
-    //   '123456': isPalyndrome('123456'),
-    // })
-    // console.log('---');
+    console.log('---');
+    console.log({
+      'mama': isPalindrome('mama'),
+      'mamam': isPalindrome('mamam'),
+      '123321': isPalindrome('123321'),
+      '123456': isPalindrome('123456'),
+        'TacoCat': isPalindrome('TacoCat'),
+        'RaceCar': isPalindrome('RaceCar'),
+    })
+    console.log('---');
 }
 console.groupEnd();
 console.log();
@@ -637,13 +649,46 @@ console.group("30. Sukurkite funkciją, kuri taiso pastraipos klaidas");
           5. Sujungti sakinius su atitinkamais sakinių skiriamaisiais/baigiamaisiais ženklais
           6. Grąžinti rezultatą.
         */
+
+        // remove spaces before and after paragraph and split paragraph in to sentences
+        paragraph = paragraph.trim().split(/([.?!])/g)
+
+        // filter for empty strings and capitalize first letter of every sentence
+        paragraph = paragraph.filter((sentence) => sentence !== '')
+            .map((string) => string[0].toUpperCase() + string.slice(1));
+
+        // join sentences to paragraph
+        paragraph = paragraph.join('');
+
+        // remove multiple spaces
+        paragraph = paragraph.replace(/\s+/g, ' ');
+
+        // look for space followed by separator and remove space before it
+        paragraph = paragraph.replace(/\s([,.?!])/g, '$1');
+
+        // change name jonas to Jonas
+        paragraph = paragraph.replace(/jonas/g, 'Jonas');
+
+        return paragraph;
+
+        // alternate "one liner" ,but barely readable:
+
+        // return paragraph
+        //     .trim()
+        //     .split(/([.?!])/g)
+        //     .filter((sentence) => sentence !== '')
+        //     .map((string) => string[0].toUpperCase() + string.slice(1)).join('')
+        //     .replace(/\s+/g, ' ')
+        //     .replace(/\s([,.?!])/g, '$1')
+        //     .replace(/jonas/g, 'Jonas')
+
     }
 
-    // const paragraph = '    labas , as jonas . Tave      vadina Kęstu? Taip ir žinojau  !    ';
-    // const fixedParagraph = fixParagraph(paragraph);
-    // console.log(paragraph);
-    // console.log(fixedParagraph);
-    // console.log('---');
+    const paragraph = '    labas , as jonas . Tave      vadina Kęstu? Taip ir žinojau  !    ';
+    const fixedParagraph = fixParagraph(paragraph);
+    console.log(paragraph);
+    console.log(fixedParagraph);
+    console.log('---');
 }
 console.groupEnd();
 console.log();
@@ -653,15 +698,15 @@ console.log();
 console.group("31. Sukurkite funkciją, kuri pirmu parametru priima sakinį ir sudeda atskirus sakinio žodžius į masyvą");
 {
     function splitSentence(sentence) {
-        // code ...
+        return sentence.split(' ');
     }
 
-    // console.log('---');
-    // console.log({
-    //   'Labas aš Jonas': splitSentence('Labas aš Jonas'),
-    //   'Kėgliai yra gerai': splitSentence('Kėgliai yra gerai'),
-    // })
-    // console.log('---');
+    console.log('---');
+    console.log({
+      'Labas aš Jonas': splitSentence('Labas aš Jonas'),
+      'Kėgliai yra gerai': splitSentence('Kėgliai yra gerai'),
+    })
+    console.log('---');
 }
 console.groupEnd();
 console.log();
@@ -674,15 +719,15 @@ console.log();
 console.group("32. Sukurkite funkciją, kuri pirmu parametru priimą <string>'ą, o antruoju skiriklį");
 {
     function explode(str, separator) {
-        // code ...
+        return str.split(separator)
     }
 
-    // console.log('---');
-    // console.log({
-    //   "'Man patinka sniegas', ' '": explode('Man patinka sniegas', ' '),
-    //   "'home/about/13', '/'": explode('home/about/13', '/'),
-    // })
-    // console.log('---');
+    console.log('---');
+    console.log({
+      "'Man patinka sniegas', ' '": explode('Man patinka sniegas', ' '),
+      "'home/about/13', '/'": explode('home/about/13', '/'),
+    })
+    console.log('---');
 }
 console.groupEnd();
 console.log();
